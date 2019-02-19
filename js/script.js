075e95ec8408cef24c7fa46556b10ad3eae53ea2
LAPTOP-1V7UC29V\Eigenaar <99046620@mydavinci.nl>
2019-02-19 14:46:09 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ var gvb = document.getElementById('gvb');
 var oneens = document.getElementById('oneens');
 var vraag = document.getElementById('vraag');
 var onderwerp = document.getElementById('onderwerp');
+
+
+
+
+
 function vraag1(){
 	onderwerp.HTML = "1. Bindend referendum";
 	vraag.innerHTML = "Er moet een bindend referendum komen, waarmee burgers door het parlement aangenomen wetten kunnen tegenhouden.";
-	
+
 }
