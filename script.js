86fa5479ee9958d5ddb56c203dbb4ff4dc698de9
LAPTOP-1V7UC29V\Eigenaar <99046620@mydavinci.nl>
2019-02-22 15:27:53 +0100

    new commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var homepage = document.getElementById('homepage');
 var questions = document.getElementById('questions');
 var title = document.getElementById('question');
+var title2 = document.getElementById('title2');
 var statement = document.getElementById('statement');
 var answers = [];
 var questionCount = 0;
+var extraImportantSec = document.getElementById('extraImportantSec');
+
 
 function start() {
 	// voegt class toe aan homepage
 	homepage.classList.add("hidden");
 	questions.classList.remove("hidden");
+	extraImportantSec.classList.add("hidden");
 	setFirstQuestion();
 }
 	// Laat eerste vraag en statement zien
@@ -25,6 +29,7 @@ function saveAnswer(event){
 
 function setNextQuestion(){
 	if (questionCount + 1 == subjects.length) {
+		extraImportant();
 		return;
 	}
 	questionCount ++;
@@ -46,4 +51,17 @@ function setPreviousQuestion(){
 	title.innerHTML = subject.title;
 	statement.innerHTML = subject.statement;
 	answers.pop();
+}
+function extraImportant(){
+	questions.classList.add("hidden");
+	extraImportantSec.classList.remove("hidden");
+	title2.innerHTML = "Zijn er onderwerpen die u extra belangrijk vind?";
+	h5.innerHTML = "Aangevinkte stellingen tellen extra mee bij het berekenen van het resulaat.";
+	subjects.forEach(function(subject){
+		createOpinion(subject);
+	});
+
+	// function(subject){
+	// 	var li = document.createElement("li");
+	// }
 }
